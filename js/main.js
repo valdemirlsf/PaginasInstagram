36def2f950e974b50b0e0e436d35b9cdb775d1fa
valdemirlsf <valdemirlsf@gmail.com>
2021-11-01 12:18:15 -0300

    correção do scroll da tela de perfil

--- js/main.js
+++ js/main.js
@@ -3,13 +3,12 @@ function asd(){
     var elemento2= document.getElementById("actionbar");
     var element3 = document.getElementById("barraInferior");
     var posicao = elemento.offsetTop-window.scrollY;
-    console.log(posicao);
     if(posicao<40){
         elemento2.style.position = 'fixed';
         elemento2.style.width = '100%';
         elemento2.style.top = '0';
         elemento2.style.backgroundColor = 'white';
-        elemento.style.marginTop = '14%';
+        elemento.style.marginTop = '45px';
         element3.style.display = 'none';
     }else{
         elemento2.style.position = 'static';
