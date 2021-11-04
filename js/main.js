e85e8ffa2c6485d460e4e21ecf12ff71c9cc97c6
valdemirlsf <valdemirlsf@gmail.com>
2021-11-04 17:50:10 -0300

    Ultimas alterações

--- js/main.js
+++ js/main.js
@@ -2,14 +2,15 @@ function asd(){
     var elemento= document.getElementById('fotos');
     var elemento2= document.getElementById("actionbar");
     var element3 = document.getElementById("barraInferior");
+    var element4 = document.getElementById("barraSuperior");
     var posicao = elemento.offsetTop-window.scrollY;
-    if(posicao<40){
+    if(posicao<100){
         elemento2.style.position = 'fixed';
         elemento2.style.width = '100%';
-        elemento2.style.top = '0';
+        elemento2.style.top = '40px';
         elemento2.style.backgroundColor = 'white';
         elemento.style.marginTop = '45px';
-        element3.style.display = 'none';
+        
     }else{
         elemento2.style.position = 'static';
         elemento.style.marginTop = '0px';
